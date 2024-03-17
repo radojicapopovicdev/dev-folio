12e38f3a3ce16943928500531ff27b726223ee31
Radojica Popovic <radojicapopovic@icloud.com>
2024-03-17 20:56:09 +0100

    - About Section done, fully responsive, added framer motion

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,55 +1,22 @@
-import { motion } from 'framer-motion';
-
 const About = () => {
-  // Define the animation variants
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: { 
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.3, // Delay between each child animation
-      }
-    },
-  };
-
-  const childVariants = {
-    hidden: { opacity: 0 },
-    visible: { 
-      opacity: 1,
-      transition: {
-        duration: 1.0, // Duration of the animation
-      }
-    },
-  };
-
   return (
-    <motion.div
+    <div
       id="about-wrapper"
-      className="bg-[#6B422A] 3xl:h-[50vh] 2xl:h-[50vh] xl:h-[50vh] lg:h-[50vh] md:h-[50vh] sm:h-[50vh]"
-      variants={containerVariants}
-      initial="hidden"
-      animate="visible"
+      className="bg-[#6B422A] 3xl:h-[50vh] 2xl:h-[50vh] xl:h-[50vh] lg:h-[50vh] md:h-[50vh] sm:h-[50vh] "
     >
-      <motion.p
-        className="text-white font-karla 2xl:text-[14px] 2xl:pt-[106px] 2xl:ml-[450px]"
-        variants={childVariants}
-      >
+      <p className="text-white font-karla 2xl:text-[14px] 2xl:pt-[106px] 2xl:ml-[450px] ">
         A FEW WORDS ABOUT ME
-      </motion.p>
-      <motion.p
-        className="text-white font-karla 2xl:text-[36px] 2xl:w-[708px] 2xl:ml-[970px]"
-        variants={childVariants}
-      >
+      </p>
+      <p className="text-white font-karla 2xl:text-[36px] 2xl:w-[708px] 2xl:ml-[970px]  ">
         I AM RADOJICA POPOVIC, A SEASONED
-      </motion.p>
-      <motion.p
-        className="text-white font-karla 2xl:text-[36px] 2xl:w-[1124px] 2xl:ml-[450px]"
-        variants={childVariants}
-      >
+      </p>
+      <p className="text-white font-karla 2xl:text-[36px] 2xl:w-[1124px] 2xl:ml-[450px] ">
         SOFTWARE ENGINEER. WHILE I AM ADEPT AT HANDLING EVERY ASPECT OF
-        DEVELOPMENT CYCLE, MY SPECIALTY LIES IN <span className="text-[#EB905C] font-karla">FRONT END DEVELOPMENT</span>, PARTICULARLY WITH THE USE OF REACT IN CONJUNCTION WITH TYPESCRIPT.
-      </motion.p>
-    </motion.div>
+        DEVELOPMENT CYCLE, MY SPECIALTY LIES IN{" "}
+        <span className="text-[#EB905C] font-karla">FRONT END DEVELOPMENT</span>{" "}
+        , PARTICULARLY WITH THE USE OF REACT IN CONJUNCTION WITH TYPESCRIPT.
+      </p>
+    </div>
   );
 };
 
